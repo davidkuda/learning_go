c51f1c027fc3adf03fdc7fb131f0a66b54689e6e
David Kuda <david@kuda.ai>
2022-10-06 22:44:05 +0200

    feat: implement "-l" option to count lines

--- pragprog/go_cli/1_wc/main.go
+++ pragprog/go_cli/1_wc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "bufio"
+    "flag"
     "fmt"
     "io"
     "os"
@@ -9,16 +10,25 @@ import (
 
 
 func main() {
-    fmt.Println(count(os.Stdin))
+    lines := flag.Bool("l", false, "Count Lines")
+    flag.Parse()
+
+    num := count(os.Stdin, *lines)
+    fmt.Println(num)
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
     scanner := bufio.NewScanner(r)
-    scanner.Split(bufio.ScanWords)
+
+    if !countLines {
+        scanner.Split(bufio.ScanWords)
+    }
+
     wc := 0
     for scanner.Scan() {
         wc++
     }
+
     return wc
 }
 
--- pragprog/go_cli/1_wc/main_test.go
+++ pragprog/go_cli/1_wc/main_test.go
@@ -8,7 +8,19 @@ import (
 func TestCountWords(t *testing.T) {
     b := bytes.NewBufferString("one two three four five")
     exp := 5
-    res := count(b)
+    res := count(b, false)
+
+    if res != exp {
+        t.Errorf("Expected %d, got %d instead.\n", exp, res)
+    }
+}
+
+func TestCountLines(t *testing.T) {
+    b := bytes.NewBufferString("one\ntwo\nthree")
+
+    exp := 3
+
+    res := count(b, true)
 
     if res != exp {
         t.Errorf("Expected %d, got %d instead.\n", exp, res)
