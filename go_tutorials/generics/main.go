c9b198f394631541004b5811cd168eaf609691e6
David Kuda <david@kuda.ai>
2022-12-18 20:17:46 +0100

    use an interface with a type declaration (constraint)

--- go_tutorials/generics/main.go
+++ go_tutorials/generics/main.go
@@ -9,6 +9,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	// Initialize a map for the integer values
 	ints := map[string]int64{
@@ -31,7 +35,7 @@ func main() {
 	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats[string, int64](ints),
 		SumIntsOrFloats[string, float64](floats))
-	
+
 	// --- Remove type arguments when calling the generic function:
 
 	// In many cases, you can omit the type arguments because the compiler can infer them.
@@ -39,6 +43,14 @@ func main() {
 	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
+
+	// --- Declare a type constraint:
+	// Essentially, you’re moving the union from the function declaration into a new type constraint.
+	// That way, when you want to constrain a type parameter to either int64 or float64, you can use
+	// this Number type constraint instead of writing out int64 | float64.
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 
 // SumInts adds together the values of m.
@@ -62,6 +74,16 @@ func SumFloats(m map[string]float64) float64 {
 // SumIntsOrFloats sums the values of map m. It supports both int64 and float64
 // as types for map values.
 func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
