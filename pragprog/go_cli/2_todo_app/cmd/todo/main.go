9bf75599bbe3698e53d831bb555ed69f384bc74d
davidkuda <david@kuda.ai>
2022-12-21 13:35:10 +0100

    feat: introduce the three args "task", "list" and "complete"

--- pragprog/go_cli/2_todo_app/cmd/todo/main.go
+++ pragprog/go_cli/2_todo_app/cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"pragprog.com/rggo/interacting/todo"
 )
@@ -12,8 +12,17 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// Parse command line flags:
+	task := flag.String("task", "", "Task to be included in the Todo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// Define an items list
 	l := &todo.List{}
 
+	// Read existing data from .todo.json with Get()
 	if err := l.Get(todoFileName); err != nil {
 		// exit 1 will facilitate the use of this program in other unix scripts or progs
 		// os.Stderr is the place to output err, instead of stdout
@@ -22,17 +31,33 @@ func main() {
 	}
 
 	switch {
-		// for no extra args, print the list
-	case len(os.Args) == 1:
+	case *list:
+		// List current to do items
 		for _, item := range *l {
 			fmt.Println(item.Task)
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid Option")
+		os.Exit(1)
 	}
 }
