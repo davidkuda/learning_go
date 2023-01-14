8252eb7d912e473a62dfbd278605439634e65bce
davidkuda <david@kuda.ai>
2023-01-14 21:51:31 +0100

    feat: create func todoRouter

--- pragprog/go_cli/8_rest_api/handlers.go
+++ pragprog/go_cli/8_rest_api/handlers.go
@@ -12,3 +12,46 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	content := "There is an API here !\n"
 	replyTextContent(w, r, http.StatusOK, content)
 }
+
+func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		list := todo.List{}
+		if err := list.Get(todoFile); err != nil {
+			replyError(w, r, http.StatusInternalServerError, err.Error())
+			return
+		}
+		if r.URL.Path == "" {
+			switch r.Method {
+			case http.MethodGet:
+				getAllHandler(w, r, list)
+			case http.MethodPost:
+				addHandler(w, r, list, todoFile)
+			default:
+				message := "Method not supported"
+				replyError(w, r, http.StatusMethodNotAllowed, message)
+			}
+			return
+		}
+
+		id, err := validateID(r.URL.Path, list)
+		if err != nil {
+			if errors.Is(err, ErrNotFound) {
+				replyError(w, r, http.StatusNotFound, err.Error())
+				return
+			}
+			replyError(w, r, http.StatusBadRequest, err.Error())
+		}
+		switch r.Method {
+		case http.MethodGet:
+			getOneHandler(w, r, list, id)
+		case http.MethodDelete:
+			deleteHandler(w, r, list, id, todoFoile)
+		case http.MethodPatch:
+			patchHandler(w, r, list, id, todoFile)
+		default:
+			message := "Method not supported"
+			replyError(w, r, http.StatusMethodNotAllowed, message)
+		}
+	}
+}
+
