43a254b8d2bd6ec9bbcd653bd65df69aa2221802
davidkuda <david@kuda.ai>
2023-02-05 22:01:33 +0100

    feat: use a func to attach handlers to mux

--- PracticalGo/5_basic_http_server/server.go
+++ PracticalGo/5_basic_http_server/server.go
@@ -14,13 +14,22 @@ func main() {
 	}
 	
 	mux := http.NewServeMux()
+	setupHandlers(mux)
 	
-	mux.HandleFunc("/v1/api", apiHandler)
 	log.Fatal(http.ListenAndServe(listenAddr, mux))
+}
+
+func setupHandlers(mux *http.ServeMux) {
+	mux.HandleFunc("/v1/api", apiHandler)
+	mux.HandleFunc("/v1/healthz", healthCheckHandler)
 }
 
 // w: object to write back the response; r: incoming request
 // no return required
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Health shining like the moon!")
 }
